e10181fa4eb69afabb3d28043a8e9b909ad7283c
maui <hello@mauriciabad.com>
2024-02-07 16:22:14 +0100

    Install prettier and fix lint issues

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('No root element found')
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
