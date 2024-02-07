e2581c502feda31835d3e01f9b8cbf0baa70933c
maui <hello@mauriciabad.com>
2024-02-07 23:35:18 +0100

    Translate login

--- src/LoginProtected.tsx
+++ src/LoginProtected.tsx
@@ -3,6 +3,58 @@ import { Auth } from '@supabase/auth-ui-react'
 // import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { supabase } from './supabase'
 import { useAuth } from './hooks/useAuth'
+import { I18nVariables } from '@supabase/auth-ui-shared'
+
+const localizationVariables: I18nVariables = {
+  sign_up: {
+    email_label: 'Email',
+    password_label: 'Contraseña',
+    email_input_placeholder: 'Tu email',
+    password_input_placeholder: 'Tu contraseña',
+    button_label: 'Registrarse',
+    loading_button_label: 'Registrando ...',
+    social_provider_text: 'Continuar con {{provider}}',
+    link_text: '¿No tienes una cuenta? Regístrate',
+    confirmation_text: 'Revisa tu correo para el enlace de confirmación',
+  },
+  sign_in: {
+    email_label: 'Email',
+    password_label: 'Contraseña',
+    email_input_placeholder: 'Tu email',
+    password_input_placeholder: 'Tu contraseña',
+    button_label: 'Iniciar sesión',
+    loading_button_label: 'Iniciando sesión ...',
+    social_provider_text: 'Continuar con {{provider}}',
+    link_text: '¿Ya tienes una cuenta? Inicia sesión',
+  },
+  forgotten_password: {
+    email_label: 'Email',
+    password_label: 'Tu contraseña',
+    email_input_placeholder: 'Tu email',
+    button_label: 'Enviar instrucciones para restablecer la contraseña',
+    loading_button_label: 'Enviando instrucciones para restablecer ...',
+    link_text: '¿Olvidaste tu contraseña?',
+    confirmation_text:
+      'Revisa tu correo para el enlace de restablecimiento de contraseña',
+  },
+  update_password: {
+    password_label: 'Nueva contraseña',
+    password_input_placeholder: 'Tu nueva contraseña',
+    button_label: 'Actualizar contraseña',
+    loading_button_label: 'Actualizando contraseña ...',
+    confirmation_text: 'Tu contraseña ha sido actualizada',
+  },
+  verify_otp: {
+    email_input_label: 'Email',
+    email_input_placeholder: 'Tu email',
+    phone_input_label: 'Número de teléfono',
+    phone_input_placeholder: 'Tu número de teléfono',
+    token_input_label: 'Token',
+    token_input_placeholder: 'Tu token Otp',
+    button_label: 'Verificar token',
+    loading_button_label: 'Verificando token ...',
+  },
+}
 
 export const LoginProtected: FC<PropsWithChildren> = ({ children }) => {
   const { user } = useAuth()
@@ -20,6 +72,9 @@ export const LoginProtected: FC<PropsWithChildren> = ({ children }) => {
             input: 'input input-bordered w-full',
             label: 'label label-text',
           },
+        }}
+        localization={{
+          variables: localizationVariables,
         }}
       />
     )
