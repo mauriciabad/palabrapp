9d488827b1a807eb9072386d8cae37ca4ab166c6
maui <hello@mauriciabad.com>
2024-02-07 16:52:12 +0100

    Fix tailwind an minimal setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <h1 className="text-xl">Palabrapp</h1>
 
-      <p className="text-sm">
+      <p className="text-sm text-red-500">
         Small app for education. Spanish language students use it to track their
         vocabulary.
       </p>
