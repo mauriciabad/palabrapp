b90d423459159f708c9d6908f93d0409864c0071
maui <hello@mauriciabad.com>
2024-02-10 01:56:57 +0100

    Add record button

--- src/views/entry-new/StepSay.tsx
+++ src/views/entry-new/StepSay.tsx
@@ -1,7 +1,58 @@
 import { FC } from 'react'
+import { useVoiceRecorder } from 'use-voice-recorder'
+import { useState } from 'react'
+import { cn } from '../../utils/cn'
+import { IconMicrophone } from '@tabler/icons-react'
 
 export const StepSay: FC<{
   setStepValidity: (enabled: boolean) => void
 }> = () => {
-  return <div>Step Say</div>
+  const [record, setRecord] = useState<Blob | null>(null)
+  const { isRecording, stop, start } = useVoiceRecorder(setRecord)
+
+  return (
+    <>
+      <button
+        onMouseDown={(e) => {
+          e.preventDefault()
+          void start()
+        }}
+        onMouseUp={(e) => {
+          e.preventDefault()
+          stop()
+        }}
+        onTouchStart={(e) => {
+          e.preventDefault()
+          void start()
+        }}
+        onTouchEnd={(e) => {
+          e.preventDefault()
+          stop()
+        }}
+        className={cn(
+          'btn btn-circle btn-primary btn-lg relative',
+          isRecording && 'btn-error',
+          'before:absolute before:inset-0 before:-z-10 before:animate-ping before:rounded-full before:bg-error',
+          !isRecording && 'before:hidden',
+          'mx-auto mt-16 flex h-32 w-32',
+        )}
+      >
+        <IconMicrophone size={24 * 3} stroke={4 / 3} />
+      </button>
+      <p className="mt-4 text-center text-sm text-stone-500">
+        Manten presionado el botón para grabar.
+        <br />
+        Suelta para detener la grabación.
+      </p>
+
+      {record && (
+        <audio
+          src={window.URL.createObjectURL(record)}
+          controls
+          preload={'metadata'}
+          className="mt-8 w-full"
+        />
+      )}
+    </>
+  )
 }
