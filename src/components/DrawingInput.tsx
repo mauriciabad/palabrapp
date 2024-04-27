3f910b892d05837422ec54ab04d908abd953ffb3
maui <hello@mauriciabad.com>
2024-04-27 23:42:15 +0200

    Style canvas buttons

--- src/components/DrawingInput.tsx
+++ src/components/DrawingInput.tsx
@@ -4,6 +4,14 @@ import {
   type ReactSketchCanvasRef,
 } from 'react-sketch-canvas'
 import { cn } from '../utils/cn'
+import {
+  IconArrowBackUp,
+  IconArrowForwardUp,
+  IconEraser,
+  IconPalette,
+  IconPencil,
+  IconRestore,
+} from '@tabler/icons-react'
 
 export const DrawingInput: FC<{
   className?: string
@@ -44,102 +52,83 @@ export const DrawingInput: FC<{
     canvasRef.current?.redo()
   }
 
-  const handleClearClick = () => {
-    canvasRef.current?.clearCanvas()
-  }
-
   const handleResetClick = () => {
     canvasRef.current?.resetCanvas()
   }
 
   return (
-    <div className={cn('d-flex flex-column gap-2 p-2', className)}>
-      <h1>Tools</h1>
-      <div className="d-flex align-items-center gap-2 ">
-        <label htmlFor="strokeWidth" className="form-label">
-          Stroke width
-        </label>
-        <input
-          disabled={eraseMode}
-          type="range"
-          className="form-range"
-          min="1"
-          max="20"
-          step="1"
-          id="strokeWidth"
-          value={strokeWidth}
-          onChange={handleStrokeWidthChange}
-        />
-        <label htmlFor="eraserWidth" className="form-label">
-          Eraser width
-        </label>
-        <input
-          disabled={!eraseMode}
-          type="range"
-          className="form-range"
-          min="1"
-          max="20"
-          step="1"
-          id="eraserWidth"
-          value={eraserWidth}
-          onChange={handleEraserWidthChange}
-        />
-      </div>
+    <div
+      className={cn(
+        'mx-auto box-content flex flex-col items-center gap-2 p-2',
+        className,
+      )}
+    >
+      <input
+        type="range"
+        className="range w-full max-w-[320px]"
+        min="1"
+        max="20"
+        step="1"
+        value={eraseMode ? eraserWidth : strokeWidth}
+        onChange={eraseMode ? handleEraserWidthChange : handleStrokeWidthChange}
+      />
 
-      <div className="h-9 w-9 overflow-hidden rounded-full">
-        <input
-          title="Color"
-          className="h-[200%] w-[200%] -translate-x-1/4 -translate-y-1/4 transform-cpu cursor-pointer appearance-none border-none bg-transparent"
-          type="color"
-          value={strokeColor}
-          onChange={handleStrokeColorChange}
-        />
-      </div>
-      <div className="d-flex align-items-center gap-2 ">
+      <div className="flex items-center justify-between gap-2">
+        <div className="relative h-12 w-12 overflow-hidden rounded-full">
+          <input
+            title="Color"
+            className="h-[200%] w-[200%] -translate-x-1/4 -translate-y-1/4 transform-cpu cursor-pointer appearance-none border-none bg-transparent"
+            type="color"
+            value={strokeColor}
+            onChange={handleStrokeColorChange}
+          />
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
+            <IconPalette className="text-white" />
+          </div>
+        </div>
+
+        <div className="join">
+          <button
+            type="button"
+            className={cn('btn btn-circle join-item', {
+              'btn-primary': !eraseMode,
+            })}
+            onClick={handlePenClick}
+          >
+            <IconPencil />
+          </button>
+          <button
+            type="button"
+            className={cn('btn btn-circle join-item', {
+              'btn-primary': eraseMode,
+            })}
+            onClick={handleEraserClick}
+          >
+            <IconEraser />
+          </button>
+        </div>
+
         <button
           type="button"
-          className="btn-outline-primary btn btn-sm"
-          disabled={!eraseMode}
-          onClick={handlePenClick}
-        >
-          Pen
-        </button>
-        <button
-          type="button"
-          className="btn-outline-primary btn btn-sm"
-          disabled={eraseMode}
-          onClick={handleEraserClick}
-        >
-          Eraser
-        </button>
-        <div className="vr" />
-        <button
-          type="button"
-          className="btn-outline-primary btn btn-sm"
-          onClick={handleUndoClick}
-        >
-          Undo
-        </button>
-        <button
-          type="button"
-          className="btn-outline-primary btn btn-sm"
-          onClick={handleRedoClick}
-        >
-          Redo
-        </button>
-        <button
-          type="button"
-          className="btn-outline-primary btn btn-sm"
-          onClick={handleClearClick}
-        >
-          Clear
-        </button>
-        <button
-          type="button"
-          className="btn-outline-primary btn btn-sm"
+          className="btn btn-circle"
           onClick={handleResetClick}
         >
-          Reset
+          <IconRestore />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-circle"
+          onClick={handleUndoClick}
+        >
+          <IconArrowBackUp />
+        </button>
+        <button
+          type="button"
+          className="btn btn-circle"
+          onClick={handleRedoClick}
+        >
+          <IconArrowForwardUp />
         </button>
       </div>
 
@@ -151,7 +140,6 @@ export const DrawingInput: FC<{
         eraserWidth={eraserWidth}
         width="300px"
         height="300px"
-        className="mx-auto rounded-md border border-gray-300"
       />
     </div>
   )
